6aea356738e0983609ff8fbeae210f6b41d3f745
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-08 01:39:53 +0600

    Update 16_LeepYear.c

--- 16_LeepYear.c
+++ 16_LeepYear.c
@@ -2,7 +2,7 @@
 int main()
 {
     int year;
-    printf("Enter year to check it is leap or not: ");
+    printf("Enter year : ");
     scanf("%d", &year);
     if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0))
     {
